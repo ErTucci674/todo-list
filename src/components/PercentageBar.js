bd66fb273935e93fd0c7e7a69361da4165853dbf
Alessandro Amatucci Girlanda <alexanderamatucci@hotmail.com>
2025-06-26 16:31:33 +0100

    Added: settings window (currently empty)

--- src/components/PercentageBar.js
+++ src/components/PercentageBar.js
@@ -1,7 +1,14 @@
+import { useState } from 'react';
 import './PercentageBar.css';
 import SettingsIcon from './svgs/component/SettingsIcon';
+import DeleteIcon from './svgs/component/DeleteIcon';
 
 function PercentageBar({ tasksTotal, tasksCompleted }) {
+    // Settings
+    const [openSettings, setOpenSettings] = useState(false);
+    const [displaySettings, setDisplaySettings] = useState('none');
+    const settingsWindowDisplayMode = 'grid';
+
     // Access the html root variables
     const rootStyles = getComputedStyle(document.documentElement);
     const orange = rootStyles.getPropertyValue('--orange');
@@ -10,6 +17,13 @@ function PercentageBar({ tasksTotal, tasksCompleted }) {
     // Determines the when the percentage bar color changes
     // Correspond to the ratio of completed tasks over total tasks
     const factorChangeColor = 0.75;
+
+    // Toggle the settings window
+    const toggleSettings = (event) => {
+        const settingsStatus = event.target.checked;
+        settingsStatus ? setDisplaySettings(settingsWindowDisplayMode) : setDisplaySettings('none');
+        setOpenSettings(settingsStatus);
+    }
 
     // Switch colors (smooth change takes place in the css)
     let currentCompletionLineColor = orange;
@@ -30,7 +44,20 @@ function PercentageBar({ tasksTotal, tasksCompleted }) {
         <div className="PercentageBar">
             <div className='percentage-bar' style={{ '--completionStatus': factorToPercentage(), '--completionLineColor': currentCompletionLineColor }}></div>
             <div className='icon-wrapper'>
+                <input name='settings-checkbox' className='settings-checkbox' type='checkbox' checked={openSettings} onChange={toggleSettings} />
                 <SettingsIcon />
+            </div>
+
+            <div className='settings-wrapper' style={{ 'display': displaySettings }}>
+                <div className='settings-window'>
+                    <div className='top-section'>
+                        <span>Settings</span>
+                        <div className='icon-wrapper'>
+                            <input name='settings-window-checkbox' className='settings-window-checkbox' type='checkbox' checked={openSettings} onChange={toggleSettings} />
+                            <DeleteIcon />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
