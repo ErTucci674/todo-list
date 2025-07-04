be485b3eac40fb5b6fedb8fa775e9d2213d08a2c
Alessandro Amatucci Girlanda <alexanderamatucci@hotmail.com>
2025-07-04 23:32:13 +0100

    Developed - toggle switch for the app's settings

---
+++ src/components/ToggleSwitch.js
@@ -0,0 +1,12 @@
+import './ToggleSwitch.css';
+
+function ToggleSwitch() {
+    return (
+        <div className='ToggleSwitch'>
+            <input type='checkbox'></input>
+            <div className='switch-style'></div>
+        </div>
+    )
+}
+
+export default ToggleSwitch;
