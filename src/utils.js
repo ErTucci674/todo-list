2b2893d9857137550481975f509c874de7faba55
ErTucci674 <alexanderamatucci@hotmail.com>
2026-05-08 20:40:03 +0100

    New: two functions - 'word separator' in a string; 'word capitalizer' in a string

--- src/utils.js
+++ src/utils.js
@@ -7,3 +7,15 @@ export function dateToString(date) {
     if (isNaN(tmpDate.getTime())) return "No due date";
     return tmpDate.toLocaleDateString('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric' }).replace(/\//g, '.');
 }
+
+// Words are capitalized according to the string's white spaces
+export function capitalizeWords(str) {
+    const splittedStr = str.split(/\s/);
+    const tempStr = splittedStr.map(word => (word[0].toUpperCase() + word.slice(1))).join(' ');
+    return tempStr;
+}
+
+// Words are separated according to the capitalized characters in the string
+export function separateWords(str) {
+    return str.length <= 1 ? str : str.split(/(?=[A-Z])/).join(' ');
+}
