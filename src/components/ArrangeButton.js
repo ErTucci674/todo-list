145fb0f6e36c3141938361a839c1de2eeafcac67
ErTucci674 <alexanderamatucci@hotmail.com>
2026-05-08 20:38:53 +0100

    New: Filter option selectable from the corresponding Arrange Button (not active yet)

--- src/components/ArrangeButton.js
+++ src/components/ArrangeButton.js
@@ -1,18 +1,20 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { capitalizeWords, separateWords } from '../utils.js';
 import './ArrangeButton.css';
 import SortIcon from './svgs/component/SortIcon.js';
 import FilterIcon from './svgs/component/FilterIcon.js';
 
-function ArrangeButton({ sort, sorting, filter }) {
+function ArrangeButton({ sortState, sorting, filterState, filterTasks, filter }) {
     const [buttonChecked, setButtonChecked] = useState(false);
     const [dueDateChecked, setDueDateChecked] = useState(false);
     const [importanceChecked, setImportanceChecked] = useState(false);
+    const [filterOptions, setFilterOptions] = useState({});
 
     // Choose the header corresponding to the arrange button type
     let headerText = "";
-    if (sort) {
+    if (sortState) {
         headerText = "Sort by";
-    } else if (filter) {
+    } else if (filterState) {
         headerText = "Filter by"
     }
 
@@ -24,20 +26,31 @@ function ArrangeButton({ sort, sorting, filter }) {
 
         if (name === "button") {
             setButtonChecked(checked);
-        } else if (name === "dueDate") {
-            setDueDateChecked(checked);
-            if (sort) {
-                setImportanceChecked(false)
-                // Reset the 'sorting order' if the checkbox has been unchecked
-                checked ? sorting.sortTasksByDueDate() : sorting.sortReset();
-            }
-        } else if (name === "importance") {
-            setImportanceChecked(checked);
-            if (sort) {
-                setDueDateChecked(false)
+        } else {
+            if (sortState) {
+                if (name === "dueDate") {
+                    setDueDateChecked(checked);
+                    setImportanceChecked(false)
 
-                // Reset the 'sorting order' if the checkbox has been unchecked
-                checked ? sorting.sortTasksByImportance() : sorting.sortReset();
+                    // Reset the 'sorting order' if the checkbox has been unchecked
+                    checked ? sorting.sortTasksByDueDate() : sorting.sortReset();
+                } else if (name === "importance") {
+                    setImportanceChecked(checked);
+                    setDueDateChecked(false)
+
+                    // Reset the 'sorting order' if the checkbox has been unchecked
+                    checked ? sorting.sortTasksByImportance() : sorting.sortReset();
+                }
+            }
+            else if (filterState) {
+                filterTasks(name, checked);
+                setFilterOptions({
+                    ...filterOptions,
+                    [name]: {
+                        ...filterOptions[name],
+                        value: checked
+                    }
+                })
             }
         }
     }
@@ -47,21 +60,54 @@ function ArrangeButton({ sort, sorting, filter }) {
         setButtonChecked(false);
     }
 
+    useEffect(() => {
+        if (!filter) return;
+
+        // ".entries" returns a list which is converted back to object with ".fromEntries"
+        setFilterOptions(
+            Object.fromEntries(
+                Object.entries(filter).map(([key, values]) => [
+                    key,
+                    {
+                        text: capitalizeWords(separateWords(key)),
+                        value: values.state
+                    }
+                ])
+            )
+        )
+    }, []);
+
     return (
         <div className='ArrangeButton icon-wrapper'>
             <input name="button" type='checkbox' checked={buttonChecked} onChange={handleChange} onBlur={handleBlur}></input>
-            {sort ? <SortIcon /> : ""}
-            {filter ? <FilterIcon /> : ""}
+            {sortState ? <SortIcon /> : ""}
+            {filterState ? <FilterIcon /> : ""}
             <div className='options' onMouseDown={(e) => e.preventDefault()}>
                 <div><b>{headerText}</b></div>
-                <div className='option'>
-                    <span>Due date</span>
-                    <input name='dueDate' type='checkbox' checked={dueDateChecked} onChange={handleChange}></input>
-                </div>
-                <div className='option'>
-                    <span>Importance</span>
-                    <input name='importance' type='checkbox' checked={importanceChecked} onChange={handleChange}></input>
-                </div>
+                {sortState && (
+                    <>
+                        <div className='option'>
+                            <span>Due date</span>
+                            <input name='dueDate' type='checkbox' checked={dueDateChecked} onChange={handleChange}></input>
+                        </div>
+                        <div className='option'>
+                            <span>Importance</span>
+                            <input name='importance' type='checkbox' checked={importanceChecked} onChange={handleChange}></input>
+                        </div>
+                    </>
+                )}
+                {filterState && (
+                    <>
+                        {
+                            Object.entries(filterOptions).map(([key, obj]) => (
+                                <div className='option' key={key}>
+                                    <span>{obj.text}</span>
+                                    <input name={key} type='checkbox' checked={obj.value} onChange={handleChange}></input>
+                                </div>
+                            ))
+                        }
+                    </>
+                )}
             </div>
         </div>
     )
